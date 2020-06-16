bdf9379ce9bef68cb19a26fe7f3df94d2b29ee64
Chen Cao <1987caochen@gmail.com>
2020-06-15 18:33:59 -0700

    Add files via upload

---
+++ PythonModule/sUtility.pyx
@@ -0,0 +1,83 @@
+
+import scipy.signal as signal
+import numpy as np
+cimport numpy as np
+cimport cython
+@cython.boundscheck(False) # turn off bounds-checking for entire function
+@cython.wraparound(False)  # turn off negative index wrapping for entire function
+
+def self_correlate(np.ndarray[np.complex_t, ndim = 1] data, long cLength, long cInterval, normalize = False):
+
+    cdef long rlength = data.shape[0] - cInterval - cLength
+
+    if rlength <= 0:
+        raise Exception("data size is shorter than correlation length")
+
+    cdef np.ndarray[np.complex_t, ndim = 1] sumProduct = np.zeros(rlength, dtype=np.complex)
+    cdef np.ndarray[np.complex_t, ndim = 1] sumProductReal = np.zeros(rlength, dtype=np.complex)
+
+    cdef long n = 0
+
+    cdef np.ndarray[np.complex_t, ndim = 1] temp = signal.correlate(data[0: cLength],
+                                                                    data[cInterval: cInterval+cLength],
+                                                                    mode="valid")
+
+    sumProduct[n] = temp[0]
+
+    if normalize:
+        temp = signal.correlate(data[0: cLength],
+                                data[0: cLength],
+                                mode="valid")
+
+        sumProductReal[n] = temp[0]
+
+    n = n + 1
+
+    cdef np.float_t sumProductRealCross,sumProductImageCross,sumProductRealSelf
+
+    while n < rlength:
+
+        sumProductRealCross = sumProduct[n-1].get('real')
+        sumProductImageCross = sumProduct[n-1].get('imag')
+        
+
+        sumProductRealCross +=  + data[n+cLength].get('real') * data[n+cInterval+cLength].get('real') \
+                                + data[n+cLength].get('imag') * data[n+cInterval+cLength].get('imag')
+
+        sumProductImageCross += - data[n+cLength].get('real') * data[n+cInterval+cLength].get('imag') \
+                                + data[n+cLength].get('imag') * data[n+cInterval+cLength].get('real')
+
+        sumProductRealCross -=  + data[n-1].get('real') * data[n-1+cInterval].get('real') \
+                                + data[n-1].get('imag') * data[n-1+cInterval].get('imag')
+        
+        sumProductImageCross -= - data[n-1].get('real') * data[n-1+cInterval].get('imag') \
+                                + data[n-1].get('imag') * data[n-1+cInterval].get('real')
+
+        sumProduct[n] = {'real': sumProductRealCross, 'imag': sumProductImageCross}
+
+        if normalize:
+
+            sumProductRealSelf = sumProductReal[n-1].get('real')
+
+            sumProductRealSelf +=   + data[n+cLength].get('real') * data[n+cLength].get('real') + data[n+cLength].get('imag')*data[n+cLength].get('imag')
+
+            sumProductRealSelf -=   + data[n-1].get('real') * data[n-1].get('real') + data[n-1].get('imag') * data[n-1].get('imag')
+
+            sumProductReal[n] = {'real': sumProductRealSelf, 'imag': 0.0}
+
+
+
+        # sumProduct[n] = sumProduct[n-1] - data[n-1] * np.conj(data[n-1+cInterval]) + data[n+cLength] * np.conj(data[n+cInterval+cLength])
+        # sumProductReal[n] = sumProductReal[n-1] - data[n-1] * np.conj(data[n-1]) + data[n+cLength] * np.conj(data[n+cLength])
+
+        n = n + 1
+
+
+    cdef np.ndarray[np.complex_t] corr
+    
+    if normalize:
+        corr = sumProduct/sumProductReal
+    else:
+        corr = sumProduct
+
+    return corr
